5665e61d608ea3d7c0877739e2fefdea23fe8e8a
JimmyLau <kuopingl+1@gmail.com>
2019-11-24 20:58:13 +0800

    implement addTextChangedListener

--- app/src/main/java/com/example/android/trackmysleepquality/sleepquality/SleepQualityFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleepquality/SleepQualityFragment.kt
@@ -17,6 +17,8 @@
 package com.example.android.trackmysleepquality.sleepquality
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -70,14 +72,26 @@ class SleepQualityFragment : Fragment() {
         // Add an Observer to the state variable for Navigating when a Quality icon is tapped.
         sleepQualityViewModel.navigateToSleepTracker.observe(this, Observer {
             if (it == true) { // Observed state is true.
-                sleepQualityViewModel.setSleepInfo(binding.sleepInfoText.text.toString())
                 this.findNavController().navigate(
                         SleepQualityFragmentDirections.actionSleepQualityFragmentToSleepTrackerFragment())
-                Log.i("Sleep Quality Fragment", "${binding.sleepInfoText.text.toString()}")
 
                 // Reset state to make sure we only navigate once, even if the device
                 // has a configuration change.
                 sleepQualityViewModel.doneNavigating()
+            }
+        })
+
+        binding.sleepInfoText.addTextChangedListener(object : TextWatcher{
+            override fun afterTextChanged(p0: Editable?) {
+                sleepQualityViewModel.setSleepInfo(p0.toString())
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
             }
         })
 
